39caf73db648356e45fa467c72ca7afb3d3ac19f
Vanessa <vy2000@student.ubc.ca>
2022-09-29 17:03:55 -0700

    adding emissions and fig1 initial values

---
+++ boxmodel.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as sci
+
+# INITIAL VALUES
+# initial box masses    
+M1 = 725 #atmosphere
+M2 = 725 #surface water
+M3 = 3 #surface biota
+M4 = 37675 #intermediate and deep water
+M5 = 110 #short-lived biota
+M6 = 450 #long-lived biota
+M7 = 60 #litter
+M8 = 1350 #soil
+M9 = 160 #peat
+FOSSILFUELS = 10000 # 10?
+
+# fluxes between boxes
+F12 = 89
+F21 = 90
+F23 = 40
+F32 = 36
+F34 = 4 #detritus
+F42 = 42
+F24 = 38
+F15 = 110
+F51 = 55
+F61 = 0 #deforestation
+F56 = 15
+F57 = 40
+F67 = 15
+F71 = 50 #detritus decomposition
+F78 = 3
+F79 = 1
+F81 = 3
+F91 = 1
+# Ffos1 = ?
+
+# FUNCTION DEFINITIONS
+def linflux(k, M):
+    """Calculates the box flux (assuming a linear flux law)
+
+    Parameters
+    ----------
+    k: int
+        rate coefficient
+    M: int
+        mass of box (that mass is moving from)
+
+    Returns
+    -------
+    k*m: int
+        flux between boxes (F)
+
+    """
+    return k*M
+
+def ratecoeff(F,M):
+    """Calculates the box flux (assuming a linear flux law)
+
+    Parameters
+    ----------
+    F: int
+        flux between boxes
+    M: int
+        mass of box (that mass is moving from)
+
+    Returns
+    -------
+    F/M: int
+        rate coefficient (k)
+
+    """
+    return F/M
+
+def boxODE(b):
+    # how to turn constructing the ode into one function?
+    return b
+
+if __name__ == "__main__":
+    M = [M1, M2, M3, M4, M5, M6, M7, M8, M9]
+    
---
+++ emissions_plot_main.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# CONSTANTS
+#time span in years
+yr_MIN = 1800
+yr_MAX = 2150
+
+
+def emissions(yr):
+    """Function defining A2 emission scenario over the interval 1990-2100
+    extended to pre-industrial (assuming linear increase from 0 in 1850 to
+    1900) and assuming full cessation of CO_2 input at 2101
+    For additional information see http://www.grida.no/climate/ipcc/emission
+
+    Parameters
+    ----------
+    yr: 1D array
+        year range for emission scenario
+
+    Returns
+    -------
+    e: double
+        A2 emissions in a given year
+
+    """
+
+    t_yr = np.array([0, 1850, 1990, 2000, 2010, 2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100, 2110, 2120, 10000])
+    # changed last 3 variables in vector to the 2000 year emission value (previously all 0)
+    e_GtC_yr = np.array([0, 0,  6.875, 8.125, 9.375, 12.5, 14.375, 16.25, 17.5, 19.75, 21.25, 23.125, 26.25, 28.75, 8.125, 8.125, 8.125])
+
+    e = np.interp(yr, t_yr, e_GtC_yr)
+
+    return e
+
+
+if __name__ == "__main__":
+    #the function linspace makes a linearly spaced vector between the min and max years of n points (n=200 below)
+    yr = np.linspace(yr_MIN, yr_MAX, 200)
+
+    #call the function emissions
+    e = emissions(yr)
+
+    plt.plot(yr, e)
+    plt.xlabel('Time (yr')
+    plt.ylabel('CO_2 emissions (GtC/yr)')
+    plt.title('Modified IPPC scenario A2')
+
+    plt.savefig('IPPC_A2_emissions.pdf')
+    plt.close()
