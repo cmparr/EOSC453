74d610a365394269d574a2f8e12c5cfee5ad31f2
Vanessa <vy2000@student.ubc.ca>
2022-09-30 15:16:39 -0700

    typed out dMdt functions + made a test 4-box model

--- 4boxmodel.py
+++ 4boxmodel.py
@@ -6,47 +6,23 @@ import scipy.integrate as sci
 # initial box masses    
 M1 = 725 #atmosphere
 M2 = 725 #surface water
-M3 = 3 #surface biota
-M4 = 37675 #intermediate and deep water
 M5 = 110 #short-lived biota
-M6 = 450 #long-lived biota
 M7 = 60 #litter
-M8 = 1350 #soil
-M9 = 160 #peat
-MFOSSILFUELS = 10000 # 10?
 
-# fluxes between boxes
-F12 = 89
+# fluxes between boxes -> DIFFERENT FROM 'boxmodel.py' TO MAKE NET FLUX 0
+F12 = 90
 F21 = 90
-F23 = 40
-F32 = 36
-F34 = 4 #detritus
-F42 = 42
-F24 = 38
 F15 = 110
 F51 = 55
-F61 = 0 #deforestation
-F56 = 15
-F57 = 40
-F67 = 15
-F71 = 50 #detritus decomposition
-F72 = 1
-F78 = 3
-F79 = 1
-F81 = 3
-F91 = 1
-# Ffos1 = ?
+F57 = 55
+F71 = 55 #detritus decomposition
+F72 = 0
 
 # BOX SYSTEM ODEs
-dM1 = [F21, -F12, F51, -F15, F61, F71, F81, F91] #+ Ffos1
-dM2 = [F12, -F21, F42, -F24, F32, -F23, F72]
-dM3 = [F23, -F32, -F34]
-dM4 = [F34, -F42, F24]
-dM5 = [F15, -F51, -F56, -F57]
-dM6 = [F56, -F67, -F61]
-dM7 = [F57, -F71, -F72, -F78, -F79, F67]
-dM8 = [F78, -F81]
-dM9 = [F79, -F91]
+dM1 = [F21, -F12, F51, -F15, F71] #Ffos1
+dM2 = [F12, -F21, F72]
+dM5 = [F15, -F51, -F57]
+dM7 = [F57, -F71, -F72]
 
 # FUNCTION DEFINITIONS
 def linflux(k, M):
@@ -85,12 +61,8 @@ def ratecoeff(F,M):
     """
     return F/M
 
-def boxODE(b):
-    # how to turn constructing the ode into one function
-    return b
-
 if __name__ == "__main__":
-    M_init = [M1, M2, M3, M4, M5, M6, M7, M8, M9]
+    M_init = [M1, M2, M5, M7]
 
 
 
