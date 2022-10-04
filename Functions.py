0814a8cb4b0cb8d41b706a1672c7ced55e405bf7
Vanessa <vy2000@student.ubc.ca>
2022-10-04 09:35:53 -0700

    cleaned up initialize and functions, added docstrings

--- Functions.py
+++ Functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 from Initialize import *
-print(F21)
+# print(F21)
 
 ## Defining functions, 
 def emissions(yr, e_stop = True):
@@ -12,7 +12,7 @@ def emissions(yr, e_stop = True):
     Parameters
     ----------
     yr: 1D array
-        year range for emission scenario
+        Year range for emission scenario
 
     Returns
     -------
@@ -20,7 +20,6 @@ def emissions(yr, e_stop = True):
         A2 emissions in a given year
 
     """
-
     t_yr = np.array([0, 1850, 1990, 2000, 2010, 2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100, 2110, 2120, 10000])
     e_GtC_yr = np.array([0, 0,  6.875, 8.125, 9.375, 12.5, 14.375, 16.25, 17.5, 19.75, 21.25, 23.125, 26.25, 28.75, 0, 0, 0])
 
@@ -33,16 +32,19 @@ def emissions(yr, e_stop = True):
     return e
 
 def Find_k(F_in, F_out, M):
-    """
-    Finds values of linear flux constants, k, for fluxes in and out for a given set of fluxes in, out and inital masses. 
-    ## Inputs ##
+    """Finds values of linear flux constants, k, for fluxes in and out for a given set of fluxes in, out and inital masses. 
+
+    Parameters
+    ----------
     F_in: NxM array
         Array with flux-in values
     F_out: NxM array
         Array with flux-out values, positive flux values
-    M: M length vector
+    M: M-length vector
         Initial values of mass for each box
-    ## Outputs ##
+
+    Returns
+    -------
     k: NxM array
         Array of k constant values to solve M-box model. k*M gives M ODEs to solve M box model problem
     """
@@ -52,8 +54,19 @@ def Find_k(F_in, F_out, M):
     return k
 
 def MassFlux_forced(t, M):
-    """
-    Mass flux coupled ODEs for 4-box model of carbon cycle in a steady state
+    """Mass flux coupled ODEs for 4-box model of carbon cycle in a steady state # TODO is this right
+
+    Parameters
+    ----------
+    t: t-length array
+        Timespan to use for emissions
+    M: M-length array
+        Initial values of mass for each box
+
+    Returns
+    -------
+    dMdt: TODO
+        Flux equation to integrate over
     """
     ## Flux in and Flux out values, units Gt/yr, to find k values
     if len(M) == 4:
@@ -67,8 +80,18 @@ def MassFlux_forced(t, M):
     return dMdt
 
 def MassFlux_steady(t, M):
-    """
-    Unforced coupled ODEs for 4-box model of carbon cycle. 
+    """Unforced coupled ODEs for 4-box model of carbon cycle. #TODO?
+    Parameters
+    ----------
+    t: t-length array
+        Timespan to use for emissions
+    M: M-length array
+        Initial values of mass for each box
+
+    Returns
+    -------
+    dMdt: TODO
+        Flux equation to integrate over
     """
     global k_4, k_9
     ## Flux in and Flux out values, units Gt/yr, to find k values
@@ -82,6 +105,28 @@ def MassFlux_steady(t, M):
     return dMdt
 
 def rk4(fxy, x0, xf, y0, N):
+    """Runge-Kutta integration to solve odes to the 4th order
+    Parameters
+    ----------
+    fxy: function 
+        The name of the function containing f(x,y) (e.g. oneode, twoode)
+    x0: int OR float 
+        Initial values of the independent variable 
+    xf: int OR float 
+        Final values of the independent variable 
+    y0: numpy array 
+        Initial value of dependent variable at xo
+    N: int 
+        Number of intervals to use between xo and xf
+
+    Returns
+    -------
+    X: numpy array
+        Numpy array containing values of the independent variable
+    Y: 1D numpy array if one equation solved; M-D numpy array [y1(x) y2(x) ... ] for multiple (M) equations
+        The estimated dependent variable at each value of the independent variable
+        
+    """
     ####### From Mark (or luke? Not my work as you can tell) #######
     # The inputs to the function are:
     #         fxy = the name of the function containing f(x,y) (e.g. oneode, twoode)
--- Initialize.py
+++ Initialize.py
@@ -2,7 +2,10 @@ import numpy as np
 
 global Flux_in_4, Flux_out_4, Flux_in_9, Flux_out_9, M0_4, M0_9
 
-## 4 Box Model Fluxes
+## 4 BOX MODEL
+# initial masses
+M0_4 = np.array([725, 725, 110, 60]) # 4 box model M0 values 
+# fluxes between boxes
 F12_4 = 90
 F21_4 = 90
 F15_4 = 110
@@ -11,7 +14,6 @@ F57_4 = 55
 F71_4 = 55 #detritus decomposition
 F72_4 =  0
 
-M0_4 = [725, 725, 110, 60] # 4 box model M0 values 
 Flux_in_4 = np.array([[0, F21_4, F51_4, F71_4], \
                     [F12_4, 0, 0, F72_4], \
                     [F15_4, 0, 0, 0],\
@@ -21,13 +23,16 @@ Mass_Flux_total_4 = sum(Flux_in_4 - Flux_out_4)
 if sum(Mass_Flux_total_4) == 0:
     print('4 Box Net flux is 0')
 
-## 9 box model initial masses
-M0_9 = np.array([725, 725, 3, 37+625, 110, 450, 60, 1350, 160])
-## 9 box model fluxes
+
+## 9 BOX MODEL
+# initial masses
+M0_9 = np.array([725, 725, 3, 37675, 110, 450, 60, 1350, 160])
+# M0_9 = np.array([725, 725, 3, 37+625, 110, 450, 60, 1350, 160]) # TODO why is M4 37+625?
+# fluxes between boxes
 F21 = 90
 F51= 55
-F61 = 1000
-F71 = 50 
+F61 = 0 #1000 # deforestation
+F71 = 50 # detritus decomposition
 F81 = 3
 F91 = 1
 F12 = 89
@@ -36,7 +41,7 @@ F42 = 42
 F72 = 1
 F23 = 40
 F24 = 38
-F34 = 4
+F34 = 4 # detritus
 F15 = 110
 F56 = 15
 F57 = 40
@@ -53,11 +58,12 @@ Flux_out_9[0, 4] = F15; Flux_out_9[4, 5] = F56; Flux_out_9[4, 6] = F57; Flux_out
 Flux_out_9[1, 3] = F24
 
 Flux_in_9 = Flux_out_9.T
-## check for zero
+# check for zero
 Mass_Flux_total_9 = sum(Flux_in_9 - Flux_out_9)
 if sum(Mass_Flux_total_9) == 0:
     print('9 Box Net flux is 0')
 
+
 # The timespan to integrate over
-t_start = 1500; t_end= 2200; n = 200
+t_start = 1500; t_end= 2200; n = 200 # TODO want 1800-2200 time interval
 t = np.linspace(t_start, t_end, n) # some time span
